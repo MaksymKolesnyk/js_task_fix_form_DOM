46b74bfd7aafb3554d97e0f66e0f663307d697f5
MaksymKolesnyk <maksymka@icloud.com>
2024-07-24 22:31:02 -0400

    make build

---
+++ index.168f9542.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".field").forEach(function(e){var t=document.createElement("label"),l=e.lastElementChild;t.className="field-label",l.setAttribute("placeholder",l.getAttribute("name")),t.setAttribute("for",l.getAttribute("id")),t.textContent=l.getAttribute("name"),e.prepend(t)});
+//# sourceMappingURL=index.168f9542.js.map
